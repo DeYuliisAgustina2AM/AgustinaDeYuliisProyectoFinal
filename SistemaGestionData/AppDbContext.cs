f7f612a81464bb20362de29973f627d73c420ea7
DeYuliisAgustina2AM <agustina.deyuliis@alumnos.uai.edu.ar>
2024-02-22 00:34:09 -0300

    Agregar archivos de proyecto.

---
+++ SistemaGestionData/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using SistemaGestionEntities;
+
+namespace SistemaGestionData
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<ProductoVendido> ProductosVendidos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=DESKTOP-FPDJKD9\\SQLEXPRESS;Database=coderhouse;Integrated Security=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+    }
+}
+
